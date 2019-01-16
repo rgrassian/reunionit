7f8ed38201b0028f158eab9805037556f7e1e4c2
rgrassian <remi.grassian@gmail.com>
2019-01-16 15:52:30 +0100

    envoi de mail

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Unavailability;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +11,21 @@ class FrontController extends AbstractController
      * Page d'accueil de l'application.
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(\Swift_Mailer $mailer)
     {
+//        $message = (new \Swift_Message('Hello Email'))
+//            ->setFrom('margouillat.reunion.it@gmail.com')
+//            ->setTo('remi.grassian@gmail.com')
+//            ->setBody(
+//                $this->renderView(
+//                    'email/registration.html.twig',
+//                    ['name' => 'Michel']
+//                ),
+//                'text/html'
+//            )
+//        ;
+//        $mailer->send($message);
+
         return $this->render('front/index.html.twig');
     }
 }
