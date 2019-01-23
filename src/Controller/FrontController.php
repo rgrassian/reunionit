97a0c076f75a56938e86b5df8ba73e451d101748
Pascal <superparisian@gmail.com>
2019-01-23 21:46:13 +0100

    initialisation des stats et page d'accueil guest

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -25,14 +25,17 @@ class FrontController extends AbstractController
                           UnavailabilityRepository $unavailabilityRepository,
                           UserRepository $userRepository)
     {
+        // Si l'application n'a jamais été utilisée, on initialise les stats avec des valeurs par défaut.
         $totalRoomCount = $roomRepository->count([]);
-        $upcomingUnavailabilitiesCount = count($unavailabilityRepository->findUpcomingUnavailabilities());
-        $roomMaxCapacity = $roomRepository->findMaxCapacityRoom();
-        $currentUnavailability = count($unavailabilityRepository->findCurrentUnavailabilities());
+        $upcomingUnavailabilitiesCount = count($unavailabilityRepository->findUpcomingUnavailabilities())   ?? 0;
+        $roomMaxCapacity = $roomRepository->findMaxCapacityRoom()                                           ?? 0;
+        $currentUnavailability = count($unavailabilityRepository->findCurrentUnavailabilities())            ?? 0;
         $currentAvailableRoomCount = $totalRoomCount - $currentUnavailability;
-        $lastOrganiser = $unavailabilityRepository->findLastUnavailability()->getOrganiser();
-        $lastMonthOrganiser = $userRepository->findLastMonthOrganiser()[0];
-        $lastMonthGuest = $userRepository->findLastMonthGuest()[0];
+        $lastOrganiser = $unavailabilityRepository->findLastUnavailability()
+            ? $unavailabilityRepository->findLastUnavailability()->getOrganiser()
+            : $this->getUser();
+        $lastMonthOrganiser = $userRepository->findLastMonthOrganiser()[0]  ?? $this->getUser();
+        $lastMonthGuest = $userRepository->findLastMonthGuest()[0]          ?? $this->getUser();
 
         return $this->render('front/index.html.twig', [
             'totalRoomCount' => $totalRoomCount,
